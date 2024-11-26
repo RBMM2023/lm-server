5325d155e37cb8262f6eae49633c776197f94c23
stephen machin <macho1878@gmail.com>
2024-11-26 05:38:54 +0000

    FC CDN updates run build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/lm-server-client.git/',
+  base: '/lm-server-client/',
   build: {
     rollupOptions: {
       input: {
