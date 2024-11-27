7125ee2890d1a00ffa282ec55b7e29f189d902c3
stephen machin <macho1878@gmail.com>
2024-11-27 07:52:58 +0000

    removed fc script tags

--- calendar.js
+++ calendar.js
@@ -1,7 +1,7 @@
 //client/calendar.js
-import { Calendar } from './node_modules/@fullcalendar/core'; /*@fullcalendar/core*/
-import dayGridPlugin from './node_modules/@fullcalendar/daygrid' /*'@fullcalendar/daygrid'*/;
-import interactionPlugin from './node_modules/@fullcalendar/interaction' /*'@fullcalendar/interaction'*/;
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 document.addEventListener('DOMContentLoaded', () => {
   const calendarEl = document.getElementById('calendar');
